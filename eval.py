c1ec2b3255627b4a17206b018dcdd598ad652755
hyx <hyx2147483647@gmail.com>
2024-09-24 13:54:27 +0800

    Refactor eval

--- eval.py
+++ eval.py
@@ -6,23 +6,19 @@ from datetime import datetime
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from importlib.metadata import version
 
-from utils.prune import check_sparsity
-from utils.eval import eval_ppl, eval_zero_shot
-from utils.eval_utils import evaluator
-from utils.eval_humaneval import eval_humaneval
-from utils.data import get_loaders
+from utils.prune_utils import check_sparsity
+from utils.eval_utils import eval_ppl, eval_zero_shot, eval_humaneval
 
 print('torch', version('torch'))
 print('transformers', version('transformers'))
 print('accelerate', version('accelerate'))
 print('# of gpus: ', torch.cuda.device_count())
 
-def get_llm(model_name, args):
+def get_llm(args, device):
     model = AutoModelForCausalLM.from_pretrained(
-        model_name, 
+        args.model_name, 
         torch_dtype=torch.float16, 
-        low_cpu_mem_usage=True, 
-        device_map="auto"
+        device_map=device
     )
     model.seqlen = 2048
     return model
@@ -35,12 +31,10 @@ def get_args():
         choices=[
             "nlu", 
             "gsm8k",
-            "mt-bench",
             "human-eval",
         ]
     )
     parser.add_argument("--eval_ppl", action="store_true")
-    parser.add_argument('--model', type=str, help='LLaMA model')
     parser.add_argument('--model_name', type=str, help='LLaMA model')
     parser.add_argument('--seed', type=int, default=0)
     args = parser.parse_args()
@@ -53,9 +47,11 @@ def main():
     np.random.seed(args.seed)
     torch.random.manual_seed(args.seed)
 
-    print(f"loading llm model {args.model}")
-    model = get_llm(args.model, args)
-    model.eval()
+    device = torch.device("cuda:0")
+    print("use device ", device)
+
+    print(f"loading llm model {args.model_name}")
+    model = get_llm(args, device)
     tokenizer = AutoTokenizer.from_pretrained(args.model, use_fast=False)
     
     if tokenizer.eos_token is None:
@@ -64,20 +60,15 @@ def main():
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
     
-    device = torch.device("cuda:0")
-    print("use device ", device)
+    model.to(device)
     
-    # sparsity = check_sparsity(model)
-    # print(f"sparsity = {sparsity}")
-    # model.to(device)
+    sparsity = check_sparsity(model)
+    print(f"sparsity = {sparsity}")
     
     # # Get the test loader
     if args.eval_ppl:
-        _, testloader = get_loaders("wikitext2", seed=0, seqlen=model.seqlen, tokenizer=tokenizer)
-        ppl_test = evaluator(model, testloader, device, args)
+        ppl_test = eval_ppl(args, model, tokenizer, device)
         print(f"wikitext perplexity: {ppl_test}")
-
-    model.to(device)
     
     if args.task == "nlu":   
         task_list = ["boolq", "rte","hellaswag","winogrande", "arc_easy","arc_challenge", "openbookqa"]
@@ -87,7 +78,7 @@ def main():
         print("zero_shot evaluation results")
         print(results)
     elif args.task == "gsm8k":
-        task_list = [args.task]
+        task_list = ["gsm8k"]
         num_shot = 0
         results = eval_zero_shot(args.model, model, tokenizer, task_list, num_shot, False)
         print("********************************")
@@ -97,7 +88,7 @@ def main():
         eval_humaneval(args, model, tokenizer)
     else:
         raise NotImplementedError
-    
+
 
 if __name__ == '__main__':
     main()
